f9ffbc0331441385d69cd17a6a8cd5bdca992984
Anhpika <anhpika12345@gmail.com>
2026-08-27 14:47:23 +0700

    Bài 4: Avatar Upload với Preview

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,7 +1,38 @@
-// import styles from './Profile.module.scss'
+import styles from "./Profile.module.scss";
+
+import DefaultAvatar from "../../assets/images/avatar-placeholder.png";
+import { useEffect, useState } from "react";
 
 function Profile() {
-    return <div>Profile</div>;
+    const [previewUrl, setPreviewUrl] = useState(null);
+
+    useEffect(() => {
+        return () => {
+            URL.revokeObjectURL(previewUrl);
+        };
+    }, [previewUrl]);
+    
+    const handleChange = (e) => {
+        const image = e.target.files[0];
+        const previewUrl = URL.createObjectURL(image);
+        setPreviewUrl(previewUrl);
+    };
+
+    return (
+        <label>
+            <input
+                type="file"
+                accept="image/*"
+                hidden
+                onChange={handleChange}
+            />
+            <img
+                className={styles.avatar}
+                src={previewUrl || DefaultAvatar}
+                alt="avatar"
+            />
+        </label>
+    );
 }
 
 export default Profile;
